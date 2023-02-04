7316adf70285611ede776a8905e94637282873cd
dapiroy <dapiroy007@gmail.com>
2023-02-04 14:57:21 -0500

    main.tf modified

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ terraform {
     }
   }
 
+  terraform {
+  cloud {
+    organization = "dapiroy"
+
+    workspaces {
+      name = "AWS-dapiroy"
+    }
+  }
+}
+
 
 resource "aws_instance" "myec2" {
    ami = "ami-0aa7d40eeae50c9a9"
